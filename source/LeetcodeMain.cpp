1a24a911548a145b1a7daca8b969751a244aad15
Chao <chao.li.arthur@gmail.com>
2019-01-28 15:51:20 +0100

    add TwoSum
    add TwoSum solution1

---
+++ source/LeetcodeMain.cpp
@@ -0,0 +1,23 @@
+/*
+ * LeetcodeMain.cpp
+ *
+ *  Created on: Jan 28, 2019
+ *      Author: root
+ */
+
+#include <cstdio>
+#include <iostream>
+
+static auto x = []()
+{
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(NULL);
+  return 0;
+}();
+
+int main(int argc, char* argv[])
+{
+
+  return 0;
+}
+
