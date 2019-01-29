0e57e5ffbdca086b76d9c5bf4e347d310bf9286f
Chao <chao.li.arthur@gmail.com>
2019-01-29 13:35:22 +0100

    Add gtest for TwoSum
    finish the TwoSum and add gtest

--- include/TwoSumSolution.h
+++ include/TwoSumSolution.h
@@ -9,6 +9,9 @@
 //Because nums[0] + nums[1] = 2 + 7 = 9,
 //return [0, 1].
 #include<vector>
+#include<unordered_map>
+#include <gtest/gtest.h>
+
 using namespace std;
 
 class TwoSumSolution1
@@ -16,7 +19,59 @@ class TwoSumSolution1
  public:
   vector<int> twoSum(vector<int>& nums, int target)
   {
+    unordered_map<int, int> num_map;
+    for(size_t i = 0; i < nums.size(); ++i)
+    {
+      num_map[nums[i]] = i;
+    }
     vector<int> result;
-    return result;
+    for(size_t i = 0; i < nums.size(); ++i)
+    {
+      int gap = target - nums[i];
+      if(num_map.find(gap) != num_map.end() && (unsigned int)num_map[gap] != i)
+      {
+        result.push_back(i);
+        result.push_back(num_map[gap]);
+        return result;
+      }
+    }
+    throw "there is no TwoSum solution!";
   }
 };
+
+//Run TwoSum solution
+void runTwoSum()
+{
+  TwoSumSolution1 ts;
+  int target = 9;
+  vector<int> nums;
+  nums.push_back(2);
+  nums.push_back(5);
+  nums.push_back(7);
+  nums.push_back(11);
+  vector<int> result;
+  try
+  {
+    result = ts.twoSum(nums, target);
+  }
+  catch (string &i)
+  {
+    std::abort();
+  }
+  printf("the TwoSum solution is: [ ");
+  for(size_t i = 0; i < result.size(); ++i)
+  {
+    printf("%d ", result[i]);
+  }
+  printf("]\n");
+}
+
+//Goole Test cases
+TEST(TwoSum_Test, testVec)
+{
+  int target = 9;
+  vector<int> nums {2, 5, 7, 11};
+  vector<int> result {0, 2};
+  TwoSumSolution1 ts;
+  EXPECT_EQ(ts.twoSum(nums, target), result);
+}
--- source/LeetcodeMain.cpp
+++ source/LeetcodeMain.cpp
@@ -2,22 +2,24 @@
  * LeetcodeMain.cpp
  *
  *  Created on: Jan 28, 2019
- *      Author: root
+ *      Author: chao
  */
 
 #include <cstdio>
+#include <cstdlib>
 #include <iostream>
+#include <string>
 
-static auto x = []()
-{
-  std::ios::sync_with_stdio(false);
-  std::cin.tie(NULL);
-  return 0;
-}();
+#include "../include/TwoSumSolution.h"
+
+
 
 int main(int argc, char* argv[])
 {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(NULL);
 
-  return 0;
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
 
