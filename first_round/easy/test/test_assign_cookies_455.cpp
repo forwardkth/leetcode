8b6b04902d120f963996ca9fa9a404de2b39d742
Chao Li <chao.li.2@volvo.com>
2021-10-31 23:27:15 +0100

    add greedy 455

---
+++ first_round/easy/test/test_assign_cookies_455.cpp
@@ -0,0 +1,20 @@
+#include "../include/assign_cookies_455.h"
+#include <gtest/gtest.h>
+
+//Google Test cases
+TEST(assign_cookies_455_test, Basics)
+{
+  vector<int> g1{1, 2, 3};
+  vector<int> s1{1, 1};
+  vector<int> g2{1, 2};
+  vector<int> s2{1, 2, 3};
+  Solution st;
+  EXPECT_EQ(st.findContentChildren(g1, s1), 1);
+  EXPECT_EQ(st.findContentChildren(g2, s2), 2);
+}
+
+int main(int argc, char **argv)
+{
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
