aef5c1e147c68ff1eb0064f6fbd98ad2c5753630
Chao Li <chao.li.arthur@gmail.com>
2023-03-10 12:38:50 +0100

    add eigen template

--- cv_src/cv_test1/cv_test1.cpp
+++ cv_src/cv_test1/cv_test1.cpp
@@ -1,18 +1,23 @@
-#include "opencv2/opencv.hpp"
+#if __INTELLISENSE__
+#undef __ARM_NEON
+#undef __ARM_NEON__
+#endif
+// https://github.com/microsoft/vscode-cpptools/issues/7413
+
 #include <iostream>
- 
+#include <Eigen/Dense>
+#include <opencv2/opencv.hpp>
 using namespace std;
 using namespace cv;
- 
-int main(int argc, char **argv){
- 
+
+void capImages(char **argv) {
   // Create a VideoCapture object and open the input file
   // If the input is the web camera, pass 0 instead of the video file name
   VideoCapture cap(argv[1]); 
   // Check if camera opened successfully
   if(!cap.isOpened()){
     cout << "Error opening video stream or file" << endl;
-    return -1;
+    return;
   }
      
   while(1){
@@ -38,6 +43,29 @@ int main(int argc, char **argv){
  
   // Closes all the frames
   destroyAllWindows();
-     
+}
+
+void eigenTest() {
+  Eigen::Matrix<int, 2, 3> matrix_23 {{2, 3, 4},
+                                      {5, 6, 7}};
+  cout << matrix_23 << endl;
+  matrix_23 << 1, 2, 3, 4, 5, 6;
+  Eigen::Matrix<int, 1, 5> c = {1, 2, 3, 4, 5};
+  Eigen::MatrixXd m(2, 2);
+  Eigen::Matrix3d mat_33 = Eigen::Matrix3d::Random();
+  Eigen::Matrix2f matrix_22 {{1, 2}, 
+                             {4, 5}};
+  cout << matrix_23 << endl;
+  cout << mat_33.transpose() << endl;
+  cout << mat_33.inverse() << endl;
+  cout << matrix_22.inverse() << endl;
+  cout << matrix_22.inverse() * matrix_22 << endl;
+}
+ 
+int main(int argc, char **argv){
+  // capImages(argv);
+  
+  // eigen test
+  eigenTest();
   return 0;
 }
